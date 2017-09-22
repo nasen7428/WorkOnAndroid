9b46cc2e61f388a87c6e18a0b1ccb1df9698ad97
nasen7428 <nasen7428@outlook.com>
2017-09-22 16:39:14 +0800

    why not save???

---
+++ vim/initvim.sh
@@ -0,0 +1,3 @@
+#! sh
+mkdir -p ~/.vim/autoload ~/.vim/bundle 
+git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
---
+++ vim/installtest.sh
@@ -0,0 +1,2 @@
+cd ~/.vim/bundle
+git clone https://github.com/tpope/vim-sensible.git 
