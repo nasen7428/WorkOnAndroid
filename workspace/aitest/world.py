9b4e1bcacdb7f1bd7271965abf2b866038303ff4
nasen7428 <nasen7428@outlook.com>
2017-09-22 08:46:58 +0800

    make some other in this git

---
+++ workspace/aitest/world.py
@@ -0,0 +1,32 @@
+#! python
+
+#this is description about how to run the world.
+#It better can be use args to define some var.
+
+import sys
+
+def help():
+    print """
+
+    -an [num],define how much attribute in this word.
+    -sn [aindex] [num],define how much status on this attribute who index is aindex.
+    -show [dbfile],show the world about how tu run.
+    
+    
+    """
+
+def initdb():
+    """
+
+    test db is exit.
+    it make sure db exit and can create dbfile.
+    if db not exit,it will install some can usefull db.
+
+
+    """
+
+
+def main():
+
+
+main()
