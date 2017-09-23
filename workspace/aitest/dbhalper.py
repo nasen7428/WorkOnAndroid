caa902d241b70995d21530d80e3100a6ba8785f1
nasen7428 <nasen7428@outlook.com>
2017-09-23 17:23:04 +0800

    fix some py file

---
+++ workspace/aitest/dbhalper.py
@@ -0,0 +1,18 @@
+#! python2
+
+
+
+def clear():
+    #clearl all db data table
+
+
+
+def insert(insertdata):
+    #insert data into db
+
+
+
+def search(states):
+    #search all rul if like states
+
+
--- workspace/aitest/world.py
+++ workspace/aitest/world.py
@@ -9,11 +9,10 @@ import os
 
 def help():
     print """
+    python2 world.py [command] [-type] [args]
+    command :
+        
 
-    -an [num],define how much attribute in this word.
-    -sn [aindex] [num],define how much status on this attribute who index is aindex.
-    -show [dbfile],show the world about how tu run.
-    
     
     """
 
@@ -43,13 +42,48 @@ def show_how_to_writ_db_file():
     attrid,stateid,attr0state,attr1state,..
     ....
     
-    """
-
-def load(data,filepath):
-    #load data from file.
+"""
 
 
-def save(data,filepath):
-    #save data to file.
+
+
+#command : .py rule clear
+#clear db ruls...
+def clear():
+    dbhalper.clear()
+
+
+#command : .py rule add 
+#add new rul into db
+def add(aid,sid,fs):
+    #aid attribute id
+    #sid state id
+    #fs [a0s,a1s,a2s,....]
+    insdata=(aid,sid,fs)
+    #insdata [aid,sid,a0s,a1s,a2s...]
+    dbhalper.insert(insdata)
+
+
+
+#command : .py rule search
+#this func will search rul table and return new state changes
+def search(states):
+    #search table and find what will change .
+    aidsids=dbhalper.search(states)
+    #states [fas1,fas2,fas3...]
+    #aidsids [(aid,sid),(aid,sid),...]
+    #define -1 is not any states.
+    #define max states number is MSNUM.
+    res=[MSNUM]
+    for re in res:
+        re=-1
+    for aidsid in aidsids:
+        res[aidsid[0]]=aidsid[1]
+    #create new states 
+    return res
+
+
+
+
 
 
